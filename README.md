bf50e11beaeb415b4767250550b50c2d63f86f59
kyle-ssg <kyle@solidstategroup.com>
2021-05-28 17:06:25 +0100

    Namespaced mocks

--- README.md
+++ README.md
@@ -2,16 +2,10 @@
 
 # E2E Proxy
 
-In order to maximise coverage via end-to-end automated tests on web and mobile, this small library acts as a proxy where you can queue mocks for errors and responses.
+In order to maximise coverage via end to end automated tests on web and mobile, this small library acts as a proxy where you can queue mocks for errors and responses.
 
 
-# Install within your project
-
-```
-npm i e2e-proxy --save
-```
-
-# Or install globally
+# Install
 
 ```
 npm i e2e-proxy --save
@@ -27,51 +21,53 @@ e2e-proxy
 
 # Example usage
 
-By default this runs a server at localhost:5000, you can override this by specifying a PORT environment variable.
-
-
-
 ## Make a request to https://jsonplaceholder.typicode.com/todos/1
-
-If no errors or mocks are queued this acts as a proxy.
 
 ```
 curl --location --request GET 'http://localhost:5000/?url=https://jsonplaceholder.typicode.com/todos/1'
 ```
 
 
-## Return an error for the next request to https://jsonplaceholder.typicode.com/todos/1
 
-This will queue an error response for the specified url.
+## Return an error for the next request to https://jsonplaceholder.typicode.com/todos/1
 
 ```
 curl --location --request POST 'http://localhost:5000/?url=todos/1&baseUrl=https://jsonplaceholder.typicode.com/&error=1' \
 --header 'Content-Type: text/plain' \
 --data-raw '{"error":"Mocked error"}'
-```
-
-Requesting it will then return the specified error.
-
-```
+# This next request will return the specified error
 curl --location --request GET 'http://localhost:5000/?url=https://jsonplaceholder.typicode.com/todos/1&baseUrl=https://jsonplaceholder.typicode.com/'
 ```
 
 
 ## Return a specific response for the next request to https://jsonplaceholder.typicode.com/todos/1
 
-This will queue a mock 200 response for the specified url.
-
-
 ```
 curl --location --request POST 'http://localhost:5000/?url=todos/1&baseUrl=https://jsonplaceholder.typicode.com/&mock=1' \
 --header 'Content-Type: text/plain' \
 --data-raw '{"userId": 2,"id": 2,"title": "test data","completed": false}'
-```
-
-Requesting it will then return the specified response.
-
-```
+# This next request will return the specified error
 curl --location --request GET 'http://localhost:5000/?url=https://jsonplaceholder.typicode.com/todos/1&baseUrl=https://jsonplaceholder.typicode.com/'
 ```
 
+# Dealing with concurrent requests
 
+If you need to mock requests for the same URL concurrently you can specify a namespace for mocks, this will ensure that race conditions do not consume any mocks or errors.
+
+```
+curl --location --request POST 'http://localhost:5000/?namespace=test1&url=todos/1&baseUrl=https://jsonplaceholder.typicode.com/&mock=1' \
+--header 'Content-Type: text/plain' \
+--data-raw '{"userId": 2,"id": 2,"title": "test data","completed": false}'
+# This next request will return the specified error
+curl --location --request GET 'http://localhost:5000/?namespace=test1&url=https://jsonplaceholder.typicode.com/todos/1&baseUrl=https://jsonplaceholder.typicode.com/'
+```
+
+As can be seen with the following, requesting with a different namespace will not return the mocked data.
+
+```
+curl --location --request POST 'http://localhost:5000/?namespace=test1&url=todos/1&baseUrl=https://jsonplaceholder.typicode.com/&mock=1' \
+--header 'Content-Type: text/plain' \
+--data-raw '{"userId": 2,"id": 2,"title": "test data","completed": false}'
+# This next request will return the specified error
+curl --location --request GET 'http://localhost:5000/?namespace=test2&url=https://jsonplaceholder.typicode.com/todos/1&baseUrl=https://jsonplaceholder.typicode.com/'
+```
--- index.js
+++ index.js
@@ -11,37 +11,45 @@ let API_MOCK_MOCKS = {};
 let API_MOCK_ERRORS = {};
 
 
-function mockNextRequest(url, body) {
-  API_MOCK_MOCKS[`${trimChar(url,"/")}`] = body
+function mockNextRequest(url, body, namespace) {
+  const theNamespace = namespace || "default";
+  API_MOCK_MOCKS[theNamespace] = API_MOCK_MOCKS[theNamespace] || {}
+  API_MOCK_MOCKS[theNamespace][`${trimChar(url,"/")}`] = body
 }
-function errorNextRequest(url,body) {
-  console.log(url)
-  API_MOCK_ERRORS[`${trimChar(url,"/")}`] = body
+function errorNextRequest(url,body, namespace) {
+  const theNamespace = namespace || "default";
+  API_MOCK_ERRORS[theNamespace] = API_MOCK_ERRORS[theNamespace] || {}
+  API_MOCK_ERRORS[theNamespace][`${trimChar(url,"/")}`] = body
 }
 
 function proxyRequest( params, method, headers,body) {
   const { baseUrl } = params;
   const url = trimChar(`${params.url}`.split("?")[0].replace(baseUrl, ""), "/")
-
+  const namespace = params.namespace || "default";
+  const mocks = API_MOCK_MOCKS[namespace]  || {}
+  const errors = API_MOCK_ERRORS[namespace]  || {}
   if (method.toUpperCase()==="OPTIONS"){
     return Promise.resolve({ body:"{}",status:200,headers:{} })
   }
-  if (API_MOCK_MOCKS[url]) { // a mock response exists, return it
-    const res = API_MOCK_MOCKS[url]+"";
-    delete API_MOCK_MOCKS[url]
+  if (mocks[url]) {  // a mock error exists, return it
+    const res = mocks[url]+"";
+    delete mocks[url]
     return Promise.resolve({
       body: res,
       status:200,
-      headers:{}
+      headers:{
+        "content-type": "application/json"
+      }
     });
   }
-  if (API_MOCK_ERRORS[url]) {  // a mock error exists, return it
-    const res = API_MOCK_ERRORS[url]+"";
-    delete API_MOCK_ERRORS[url]
+  if (errors[url]) {  // a mock error exists, return it
+    const res = errors[url]+"";
+    delete errors[url]
     return Promise.resolve({
       body: res,
       status:500,
       headers:{
+        "content-type": "application/json"
       }
     });
   }
@@ -66,7 +74,6 @@ function proxyRequest( params, method, headers,body) {
         }// skip encoding headers
       }
 
-      console.log(response.status)
       return {
         body,
         status: response.status,
@@ -79,52 +86,50 @@ function proxyRequest( params, method, headers,body) {
 
 
 http
-  .createServer((req, res) => {
+    .createServer((req, res) => {
 
-    // Add Routes
-    const url = req.url;
-    parseBody(req)
-      .then((body)=>{
-        const params = parseParams(req.url)
+      // Add Routes
+      const url = req.url;
+      parseBody(req)
+          .then((body)=>{
+            const params = parseParams(req.url)
 
-        if (params.error) {
-          errorNextRequest(params.url,body);
-          res.writeHead(200, { "Content-Type": "text/json" });
-          res.write("{}")
-          res.end()
-          return
-        }
+            if (params.error) {
+              errorNextRequest(params.url,body, params.namespace);
+              res.writeHead(200, { "Content-Type": "text/json" });
+              res.write("{}")
+              res.end()
+              return
+            }
 
-        if (params.mock) {
-          mockNextRequest(params.url,body);
-          res.writeHead(200, { "Content-Type": "text/json" });
-          res.write("{}")
-          res.end()
-          return
-        }
+            if (params.mock) {
+              mockNextRequest(params.url,body, params.namespace);
+              res.writeHead(200, { "Content-Type": "text/json" });
+              res.write("{}")
+              res.end()
+              return
+            }
 
-        proxyRequest(params, req.method.toUpperCase(),req.headers, body).then(({
-          headers,
-          body,
-          status
-        })=>{
-          res.writeHead(req.method.toUpperCase() === "OPTIONS"? 200 : status, {
-            ...headers,
-            "access-control-allow-origin": "*",
-            "content-type":"application/json",
-            "access-control-allow-headers": "*",
-            "access-control-allow-methods": "*"
+            proxyRequest(params, req.method.toUpperCase(),req.headers, body).then(({
+                                                                                     headers,
+                                                                                     body,
+                                                                                     status
+                                                                                   })=>{
+              res.writeHead(req.method.toUpperCase() === "OPTIONS"? 200 : status, {
+                ...headers,
+                "access-control-allow-origin": "*",
+                "access-control-allow-headers": "*",
+                "access-control-allow-methods": "*"
+              })
+              res.write(body)
+              res.end()
+            })
           })
-          console.log(status, params.url)
-          res.write(body)
-          res.end()
-        })
-      })
 
-  })
-  .listen(port, () => {
-    console.log(`Server listening on port ${port}...`);
-    if (process.send) {
-      process.send({ done: true });
-    }
-  });
+    })
+    .listen(port, () => {
+      console.log(`Server listening on port ${port}...`);
+      if (process.send) {
+        process.send({ done: true });
+      }
+    });
